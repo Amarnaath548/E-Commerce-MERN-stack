a0456477b44e8af0f64f74e7c7703f3ec0bb8371
Amarnaath <balajipandian.708@gmail.com>
2025-12-03 14:13:55 +0530

    update for deploy

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME || 'dnespxsij',
-  api_key: process.env.CLOUDINARY_API_KEY || "917834181638642",
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'ngegYt3dUx7iLuxz08aSlaf9sWk',
+  cloud_name: process.env.CLOUDINARY_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY ,
+  api_secret: process.env.CLOUDINARY_API_SECRET ,
 });
 
 export default cloudinary;
